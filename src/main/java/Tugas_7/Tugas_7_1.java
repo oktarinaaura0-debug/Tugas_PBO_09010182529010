d5d810583494c022c2700994945b453051866e31
oktarinaaura0-debug <oktarinaaura0@gmail.com>
2026-09-09 22:03:58 +0700

    Implement multiple loop constructs to print days
    Refactored the main method to demonstrate different looping constructs: while-loop, do-while, and for-loop for printing days of the week.

--- src/main/java/Tugas_7/Tugas_7_1.java
+++ src/main/java/Tugas_7/Tugas_7_1.java
@@ -1,20 +1,68 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package Tugas_7;
-
-/**
- *
- * @author Ann Dwi Marjusalinah
- */
 public class Tugas_7_1 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        String days[] = {
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",
+            "Saturday",
+            "Sunday"
+        };
+
+        int i = 0;
+
+        // Menggunakan while-loop
+        while (i < days.length) {
+            System.out.println(days[i]);
+            i++;
+        }
     }
-    
+}
+
+public class Tugas_7_1 {
+
+    public static void main(String[] args) {
+
+        String days[] = {
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",
+            "Saturday",
+            "Sunday"
+        };
+
+        int i = 0;
+
+        // Menggunakan do-while
+        do {
+            System.out.println(days[i]);
+            i++;
+        } while (i < days.length);
+    }
+}
+
+public class Tugas_7_1 {
+
+    public static void main(String[] args) {
+
+        String days[] = {
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",
+            "Saturday",
+            "Sunday"
+        };
+
+        // Menggunakan for-loop
+        for (int i = 0; i < days.length; i++) {
+            System.out.println(days[i]);
+        }
+    }
 }
