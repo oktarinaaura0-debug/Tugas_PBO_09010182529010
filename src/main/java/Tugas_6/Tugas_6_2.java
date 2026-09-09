aec45591da526f17ce4a2a86f66d37260b9dc65c
oktarinaaura0-debug <oktarinaaura0@gmail.com>
2026-09-09 21:57:12 +0700

    Refactor input handling with switch-case structure
    Refactored number printing logic from if-else to switch-case.

--- src/main/java/Tugas_6/Tugas_6_2.java
+++ src/main/java/Tugas_6/Tugas_6_2.java
@@ -1,20 +1,102 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package Tugas_6;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
-/**
- *
- * @author Ann Dwi Marjusalinah
- */
 public class Tugas_6_2 {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(
+                new InputStreamReader(System.in));
+
+        System.out.print("Masukkan angka 1-10: ");
+        int angka = Integer.parseInt(br.readLine());
+
+        if (angka == 1) {
+            System.out.println("Satu");
+        } else if (angka == 2) {
+            System.out.println("Dua");
+        } else if (angka == 3) {
+            System.out.println("Tiga");
+        } else if (angka == 4) {
+            System.out.println("Empat");
+        } else if (angka == 5) {
+            System.out.println("Lima");
+        } else if (angka == 6) {
+            System.out.println("Enam");
+        } else if (angka == 7) {
+            System.out.println("Tujuh");
+        } else if (angka == 8) {
+            System.out.println("Delapan");
+        } else if (angka == 9) {
+            System.out.println("Sembilan");
+        } else if (angka == 10) {
+            System.out.println("Sepuluh");
+        } else {
+            System.out.println("Invalid number");
+        }
     }
-    
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Tugas_6_2 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(
+                new InputStreamReader(System.in));
+
+        System.out.print("Masukkan angka 1-10: ");
+        int angka = Integer.parseInt(br.readLine());
+
+        switch (angka) {
+            case 1:
+                System.out.println("Satu");
+                break;
+
+            case 2:
+                System.out.println("Dua");
+                break;
+
+            case 3:
+                System.out.println("Tiga");
+                break;
+
+            case 4:
+                System.out.println("Empat");
+                break;
+
+            case 5:
+                System.out.println("Lima");
+                break;
+
+            case 6:
+                System.out.println("Enam");
+                break;
+
+            case 7:
+                System.out.println("Tujuh");
+                break;
+
+            case 8:
+                System.out.println("Delapan");
+                break;
+
+            case 9:
+                System.out.println("Sembilan");
+                break;
+
+            case 10:
+                System.out.println("Sepuluh");
+                break;
+
+            default:
+                System.out.println("Invalid number");
+                break;
+        }
+    }
+}
+
 }
