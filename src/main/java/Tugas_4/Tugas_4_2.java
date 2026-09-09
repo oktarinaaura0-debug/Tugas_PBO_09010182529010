fc007ee5756c1c035ced9e33bbb396ec85804b76
oktarinaaura0-debug <oktarinaaura0@gmail.com>
2026-09-09 21:33:12 +0700

    Update Tugas_4_2.java

--- src/main/java/Tugas_4/Tugas_4_2.java
+++ src/main/java/Tugas_4/Tugas_4_2.java
@@ -1,20 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package Tugas_4;
-
-/**
- *
- * @author Ann Dwi Marjusalinah
- */
 public class Tugas_4_2 {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        int number1 = 10;
+        int number2 = 20;
+        int number3 = 45;
+
+        int rataRata = (number1 + number2 + number3) / 3;
+
+        System.out.println("number 1 = " + number1);
+        System.out.println("number 2 = " + number2);
+        System.out.println("number 3 = " + number3);
+        System.out.println("Rata-rata = " + rataRata);
     }
-    
 }
