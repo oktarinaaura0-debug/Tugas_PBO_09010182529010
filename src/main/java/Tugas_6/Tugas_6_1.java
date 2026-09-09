80cc361dcb72866f0d32bb283171a50e2e5ca8b1
oktarinaaura0-debug <oktarinaaura0@gmail.com>
2026-09-09 21:55:18 +0700

    Refactor input/output to use JOptionPane
    Refactored the program to use JOptionPane for input and output instead of BufferedReader and System.out.

--- src/main/java/Tugas_6/Tugas_6_1.java
+++ src/main/java/Tugas_6/Tugas_6_1.java
@@ -1,20 +1,62 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package Tugas_6;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
-/**
- *
- * @author Ann Dwi Marjusalinah
- */
 public class Tugas_6_1 {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(
+                new InputStreamReader(System.in));
+
+        System.out.print("Masukkan nilai ujian 1: ");
+        double nilai1 = Double.parseDouble(br.readLine());
+
+        System.out.print("Masukkan nilai ujian 2: ");
+        double nilai2 = Double.parseDouble(br.readLine());
+
+        System.out.print("Masukkan nilai ujian 3: ");
+        double nilai3 = Double.parseDouble(br.readLine());
+
+        double rataRata = (nilai1 + nilai2 + nilai3) / 3;
+
+        System.out.println("Rata-rata = " + rataRata);
+
+        if (rataRata >= 60) {
+            System.out.println(":)");
+        } else {
+            System.out.println(":(");
+        }
     }
-    
+
+import javax.swing.JOptionPane;
+
+public class Tugas_6_1 {
+
+    public static void main(String[] args) {
+
+        double nilai1 = Double.parseDouble(
+                JOptionPane.showInputDialog("Masukkan nilai ujian 1:"));
+
+        double nilai2 = Double.parseDouble(
+                JOptionPane.showInputDialog("Masukkan nilai ujian 2:"));
+
+        double nilai3 = Double.parseDouble(
+                JOptionPane.showInputDialog("Masukkan nilai ujian 3:"));
+
+        double rataRata = (nilai1 + nilai2 + nilai3) / 3;
+
+        if (rataRata >= 60) {
+            JOptionPane.showMessageDialog(
+                    null,
+                    "Rata-rata = " + rataRata + "\n:)"
+            );
+        } else {
+            JOptionPane.showMessageDialog(
+                    null,
+                    "Rata-rata = " + rataRata + "\n:("
+            );
+        }
+    }
+}
 }
