3f51b62099062bce794f2b044c80d816c9114e4d
oktarinaaura0-debug <oktarinaaura0@gmail.com>
2026-09-09 20:25:48 +0700

    Initial commit

---
+++ src/main/java/Tugas_5/Tugas_5_1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Tugas_5;
+
+/**
+ *
+ * @author Ann Dwi Marjusalinah
+ */
+public class Tugas_5_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/main/java/Tugas_7/Tugas_7_2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Tugas_7;
+
+/**
+ *
+ * @author Ann Dwi Marjusalinah
+ */
+public class Tugas_7_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
