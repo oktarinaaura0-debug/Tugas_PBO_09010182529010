81ca3abae45d44bf5e02e0a07cadd019f316c727
oktarinaaura0-debug <oktarinaaura0@gmail.com>
2026-09-09 21:46:29 +0700

    Update Tugas_5_2.java

--- src/main/java/Tugas_5/Tugas_5_2.java
+++ src/main/java/Tugas_5/Tugas_5_2.java
@@ -1,20 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package Tugas_5;
+import javax.swing.JOptionPane;
 
-/**
- *
- * @author Ann Dwi Marjusalinah
- */
 public class Tugas_5_2 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        String word1 = JOptionPane.showInputDialog("Enter word1:");
+        String word2 = JOptionPane.showInputDialog("Enter word2:");
+        String word3 = JOptionPane.showInputDialog("Enter word3:");
+
+        String msg = word1 + " " + word2 + " " + word3;
+
+        JOptionPane.showMessageDialog(null, msg);
     }
-    
 }
