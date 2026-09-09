9483d0de9a612d044fcb22df8c73e4d7e6d21a03
oktarinaaura0-debug <oktarinaaura0@gmail.com>
2026-09-09 21:59:48 +0700

    Refactor main method to use for loop for printing
    Refactored main method to use a for loop instead of while and do-while loops for printing.

--- src/main/java/Tugas_6/Tugas_6_3.java
+++ src/main/java/Tugas_6/Tugas_6_3.java
@@ -1,20 +1,35 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package Tugas_6;
-
-/**
- *
- * @author Ann Dwi Marjusalinah
- */
 public class Tugas_6_3 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        int i = 1;
+
+        while (i <= 100) {
+            System.out.println("Aura Oktarina");
+            i++;
+        }
     }
-    
+}
+
+public class Tugas_6_3 {
+
+    public static void main(String[] args) {
+
+        int i = 1;
+
+        do {
+            System.out.println("Aura Oktarina");
+            i++;
+        } while (i <= 100);
+    }
+}
+
+public class Tugas_6_3 {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 100; i++) {
+            System.out.println("Aura Oktarina");
+        }
+    }
 }
