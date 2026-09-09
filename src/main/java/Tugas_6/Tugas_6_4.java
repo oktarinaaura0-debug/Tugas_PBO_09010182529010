3538f17d64a5d7d98c73569aa8786f2c4541fba6
oktarinaaura0-debug <oktarinaaura0@gmail.com>
2026-09-09 22:01:48 +0700

    Refactor power calculation in Tugas_6_4.java
    Refactored the power calculation method to use do-while and for loops. Removed commented-out code and unnecessary imports.

--- src/main/java/Tugas_6/Tugas_6_4.java
+++ src/main/java/Tugas_6/Tugas_6_4.java
@@ -1,20 +1,84 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package Tugas_6;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
-/**
- *
- * @author Ann Dwi Marjusalinah
- */
 public class Tugas_6_4 {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(
+                new InputStreamReader(System.in));
+
+        System.out.print("Masukkan angka: ");
+        int angka = Integer.parseInt(br.readLine());
+
+        System.out.print("Masukkan pangkat: ");
+        int pangkat = Integer.parseInt(br.readLine());
+
+        int hasil = 1;
+        int i = 1;
+
+        while (i <= pangkat) {
+            hasil = hasil * angka;
+            i++;
+        }
+
+        System.out.println("Hasil = " + hasil);
     }
-    
+
+    import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Tugas_6_4 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(
+                new InputStreamReader(System.in));
+
+        System.out.print("Masukkan angka: ");
+        int angka = Integer.parseInt(br.readLine());
+
+        System.out.print("Masukkan pangkat: ");
+        int pangkat = Integer.parseInt(br.readLine());
+
+        int hasil = 1;
+        int i = 1;
+
+        do {
+            hasil = hasil * angka;
+            i++;
+        } while (i <= pangkat);
+
+        System.out.println("Hasil = " + hasil);
+    }
+}
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Tugas_6_4 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(
+                new InputStreamReader(System.in));
+
+        System.out.print("Masukkan angka: ");
+        int angka = Integer.parseInt(br.readLine());
+
+        System.out.print("Masukkan pangkat: ");
+        int pangkat = Integer.parseInt(br.readLine());
+
+        int hasil = 1;
+
+        for (int i = 1; i <= pangkat; i++) {
+            hasil = hasil * angka;
+        }
+
+        System.out.println("Hasil = " + hasil);
+    }
+}
 }
