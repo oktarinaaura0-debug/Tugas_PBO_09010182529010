d0c22b599d541a0cd19472c930dcc9c04f124efc
oktarinaaura0-debug <oktarinaaura0@gmail.com>
2026-09-19 15:46:49 +0700

    Add definitions for OOP concepts in Latihan91.java

---
+++ src/main/java/Tugas_9/Latihan91.java
@@ -0,0 +1,26 @@
+9.6.1 Mendefinisikan Istilah
+
+1. Class
+Class adalah struktur dasar dalam OOP yang berisi atribut/field dan method. Class digunakan sebagai cetakan untuk membuat object.
+
+2. Object
+Object adalah instance atau hasil dari sebuah class yang mempunyai data/atribut dan method untuk menjalankan suatu perilaku. 
+
+3. Instantiate
+Instantiate adalah proses membuat object atau instance dari sebuah class menggunakan operator new. Contohnya:
+String str = new String("Hello");
+
+4. Instance Variable
+Instance variable adalah variabel yang dimiliki oleh masing-masing object. 
+Setiap object mempunyai nilai variabel instance masing-masing. 
+
+5. Instance Method
+Instance method adalah method yang digunakan oleh object dan biasanya dipanggil melalui nama object, misalnya:
+str.charAt(0);
+
+6. Class Variables / Static Member Variables
+Class variable adalah variabel yang dimiliki oleh class dan menggunakan static.
+Nilainya dapat digunakan bersama oleh object-object dari class yang sama. 
+
+7. Constructor
+Constructor adalah bagian khusus dari class yang digunakan ketika membuat atau menginisialisasi sebuah object. 
